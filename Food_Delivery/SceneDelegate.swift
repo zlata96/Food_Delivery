e107117d96e84181c3f6bedd62121b8959e79154
Zlata Guseva <gusevazlata96@gmail.com>
2023-04-03 00:45:43 +0400

    Add SwiftFormat and SwiftLint

--- Food_Delivery/SceneDelegate.swift
+++ Food_Delivery/SceneDelegate.swift
@@ -1,16 +1,10 @@
-//
-//  SceneDelegate.swift
-//  Food_Delivery
-//
-//  Created by Злата Гусева on 03.04.2023.
-//
+// SceneDelegate.swift
+// Food_Delivery. Created by Zlata Guseva.
 
 import UIKit
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
-
     var window: UIWindow?
-
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
@@ -46,7 +40,4 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // Use this method to save data, release shared resources, and store enough scene-specific state information
         // to restore the scene back to its current state.
     }
-
-
 }
-
--- Food_Delivery/ViewController.swift
+++ Food_Delivery/ViewController.swift
@@ -1,19 +1,11 @@
-//
-//  ViewController.swift
-//  Food_Delivery
-//
-//  Created by Злата Гусева on 03.04.2023.
-//
+// ViewController.swift
+// Food_Delivery. Created by Zlata Guseva.
 
 import UIKit
 
 class ViewController: UIViewController {
-
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
 }
-
