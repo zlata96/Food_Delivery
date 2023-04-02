7b53581d80e8b78c58cc6466ea951b21455af3a9
Zlata Guseva <gusevazlata96@gmail.com>
2023-04-03 00:55:14 +0400

    Add colors assets

---
+++ Food_Delivery/Extensions/Colors+Extension.swift
@@ -0,0 +1,11 @@
+// Colors+Extension.swift
+// Food_Delivery. Created by Zlata Guseva.
+
+import UIKit
+
+extension UIColor {
+    static let accentColor = "accentColor"
+    static let mainBackgroundColor = "mainBackgroundColor"
+    static let mainTextColor = "mainTextColor"
+    static let unselectedTabBarItem = "unselectedTabBarItem"
+}
